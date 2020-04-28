71e0fc56ee0c09e2ce29c7e2d8ca4223dd38f2e7
Ali Zahid <ali.zahid@live.com>
2020-04-28 09:16:58 +0400

    Styles updated

--- src/components/leader.tsx
+++ src/components/leader.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react'
 
+import { username } from '../lib'
 import { LeaderT } from '../types'
 
 interface Props {
@@ -8,11 +9,13 @@ interface Props {
 
 export const Leader: FunctionComponent<Props> = ({ leader }) => (
   <article className="flex items-center leading-none mt-8">
-    <aside className="bg-teal-800 h-12 w-12 flex items-center text-white justify-center rounded-full text-xl">
+    <aside className="bg-yellow-500 h-10 w-10 flex items-center text-black font-medium justify-center rounded-full text-xl">
       {leader.rank}
     </aside>
     <div className="flex-1 ml-4">
-      <div className="text-2xl font-medium break-all">{leader.user?.name}</div>
+      <div className="text-2xl font-medium break-all">
+        {leader.user?.name ?? username()}
+      </div>
       <div className="mt-2 text-gray-700">{leader.streak} days</div>
     </div>
   </article>
